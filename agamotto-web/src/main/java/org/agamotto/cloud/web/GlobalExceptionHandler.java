041d0b3c6e809e540f20a84d6aa1071dc43d433d
e <18829011980@163.com>
2021-08-20 17:33:39 +0800

    参数校验全局异常捕获

--- agamotto-web/src/main/java/org/agamotto/cloud/web/GlobalExceptionHandler.java
+++ agamotto-web/src/main/java/org/agamotto/cloud/web/GlobalExceptionHandler.java
@@ -1,26 +1,13 @@
 package org.agamotto.cloud.web;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
 import lombok.extern.slf4j.Slf4j;
 import org.agamotto.cloud.exception.AgamottoException;
 import org.agamotto.cloud.resp.AgamottoResponse;
 import org.agamotto.cloud.resp.Ret;
 import org.springframework.beans.ConversionNotSupportedException;
 import org.springframework.beans.TypeMismatchException;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
-import org.springframework.context.MessageSourceAware;
-import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.core.annotation.AnnotatedElementUtils;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.codec.cbor.Jackson2CborDecoder;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.http.converter.HttpMessageNotWritableException;
-import org.springframework.lang.Nullable;
-import org.springframework.util.StringUtils;
 import org.springframework.validation.BindException;
 import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
@@ -32,25 +19,12 @@ import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
-import org.springframework.web.server.ResponseStatusException;
-import org.springframework.web.server.ServerWebExchange;
-import org.springframework.web.server.WebExceptionHandler;
-import org.springframework.web.server.handler.ExceptionHandlingWebHandler;
-import org.springframework.web.servlet.HandlerExceptionResolver;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
-import org.springframework.web.servlet.handler.AbstractHandlerExceptionResolver;
-import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import javax.validation.ConstraintViolationException;
 
 
 @Slf4j
@@ -101,6 +75,49 @@ public class GlobalExceptionHandler  {
         return AgamottoResponse.error(500, ex.getMessage());
     }
 
+    /**
+     * 参数校验异常(校验RequestBody类型 参数)
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseBody
+    public Ret<?> allExceptionHandler(MethodArgumentNotValidException e) {
+        log.error("", e);
+        if (e.getBindingResult().getAllErrors().size() > 0) {
+            String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+            return AgamottoResponse.error(500, errorMessage);
+        } else {
+            return AgamottoResponse.error(500, e.getMessage());
+        }
+    }
 
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseBody
+    public Ret<?> listExceptionHandler(ConstraintViolationException e) {
+        log.error("", e);
+        return AgamottoResponse.error(500, e.getMessage());
+    }
+
+
+    @ExceptionHandler(MissingServletRequestParameterException.class)
+    @ResponseBody
+    public Ret<?> inputExceptionHandler(MissingServletRequestParameterException e) {
+        log.error("", e);
+        return AgamottoResponse.error(500, e.getMessage());
+    }
+
+    /**
+     * 参数校验异常(校验form-data类型 参数)
+     */
+    @ExceptionHandler(BindException.class)
+    @ResponseBody
+    public Ret<?> allExceptionHandler(BindException e) {
+        log.error("", e);
+        if (null != e.getBindingResult().getAllErrors() && e.getBindingResult().getAllErrors().size() > 0) {
+            String errorMessage = e.getBindingResult().getAllErrors().get(0).getDefaultMessage();
+            return AgamottoResponse.error(500, errorMessage);
+        } else {
+            return AgamottoResponse.error(500, e.getMessage());
+        }
+    }
 
 }

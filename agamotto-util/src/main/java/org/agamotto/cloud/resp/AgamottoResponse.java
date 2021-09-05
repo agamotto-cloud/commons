e9d6672402f73e169ffbc145f842438692faf475
董文强 <82612946@qq.com>
2021-09-05 14:10:07 +0800

    修改依赖

--- agamotto-util/src/main/java/org/agamotto/cloud/resp/AgamottoResponse.java
+++ agamotto-util/src/main/java/org/agamotto/cloud/resp/AgamottoResponse.java
@@ -1,7 +1,6 @@
 package org.agamotto.cloud.resp;
 
 
-import com.baomidou.mybatisplus.core.metadata.IPage;
 
 public class AgamottoResponse {
 
@@ -23,20 +22,7 @@ public class AgamottoResponse {
         return ret;
     }
 
-    public static <T> Ret<PageData<T>> ok(IPage<T> data) {
-        Ret<PageData<T>> ret = new Ret<>();
-        PageData<T> pageData = new PageData<>();
-        pageData.setCurrent(data.getCurrent());
-        pageData.setPages(data.getPages());
-        pageData.setSize(data.getSize());
-        pageData.setTotal(data.getTotal());
-        pageData.setData(data.getRecords());
-        ret.setData(pageData);
-        ret.setMsg("ok");
-        ret.setSuccess(true);
-        ret.setStatusCode(200);
-        return ret;
-    }
+
 
 
     public static <T> Ret<T> ok(T data, String msg) {
